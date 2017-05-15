a6c14863cf6c1458b49b001b1f4ca3e2864cdb07
Michal Stefanczuk <mstefanczuk@bluesoft.net.pl>
2017-05-15 23:22:58 +0200

    Initial commit

---
+++ src/main/java/pl/stefanprogramuje/controller/ContactController.java
@@ -0,0 +1,34 @@
+package pl.stefanprogramuje.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/")
+public class ContactController {
+
+    private ContactRepository contactRepository;
+
+    @Autowired
+    public ContactController(ContactRepository contactRepository) {
+        this.contactRepository = contactRepository;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String home(Map<String, Object> model) {
+        List<Cotnact> contacts = contactRepository.findAll();
+        model.put("contacts", contacts);
+        return "home";
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    public String submit(Contact contact) {
+        contactRepository.save(contact);
+        return "redirect:/";
+    }
+}
