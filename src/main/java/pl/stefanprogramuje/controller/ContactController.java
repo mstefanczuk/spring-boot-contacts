be7a73941df5c2c1fe3f5876bb886da57cb502fe
Michal Stefanczuk <mstefanczuk@bluesoft.net.pl>
2017-05-15 23:45:34 +0200

    Add domain class Contact

--- src/main/java/pl/stefanprogramuje/controller/ContactController.java
+++ src/main/java/pl/stefanprogramuje/controller/ContactController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import pl.stefanprogramuje.domain.Contact;
 
 import java.util.List;
 import java.util.Map;
@@ -21,7 +22,7 @@ public class ContactController {
 
     @RequestMapping(method = RequestMethod.GET)
     public String home(Map<String, Object> model) {
-        List<Cotnact> contacts = contactRepository.findAll();
+        List<Contact> contacts = contactRepository.findAll();
         model.put("contacts", contacts);
         return "home";
     }
